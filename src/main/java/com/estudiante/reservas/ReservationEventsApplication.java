2c76fa5d9ccb2c8c25a9d1d87cf76a2c5402727b
Jonathan Moreta <107773598+JonathanMMG@users.noreply.github.com>
2026-07-10 17:47:10 -0500

    chore: inicializa proyecto spring webflux

---
+++ src/main/java/com/estudiante/reservas/ReservationEventsApplication.java
@@ -0,0 +1,12 @@
+package com.estudiante.reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationEventsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/estudiante/reservas/ReservationEventsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.estudiante.reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
